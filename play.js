d7ba247239a7e827de9f2c7b27cada9398540e7d
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-15 08:35:01 -0400

    complete stdin set up assignment

--- play.js
+++ play.js
@@ -1,4 +1,22 @@
 const connect = require("./client");
 
 console.log(`Connecting`);
+
+const setUpInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data",handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function(key) {
+  if (key === "\u0003") {
+    process.exit();
+  }
+};
+
 connect();
+setUpInput();
