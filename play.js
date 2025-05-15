08672e110eb39a834240cd8cfec4692f3f42fe91
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-14 23:22:01 -0400

    Fix linting errors

--- play.js
+++ play.js
@@ -1,15 +1,15 @@
 const net = require("net");
-const connect = function(){
+const connect = function() {
   const conn = net.createConnection({
     host:"localhost",
     port:50541
-  })
+  });
   conn.setEncoding("utf8");
 
   conn.on("data",(data)=>{
     console.log(`Data for player:`, data);
-  })
+  });
   return conn;
-}
+};
 console.log(`Connecting`);
 connect();
