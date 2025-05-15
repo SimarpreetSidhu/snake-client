f46a877dd72a2d49687ad1c5bc481188312cc48b
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-15 08:14:02 -0400

    complete move commands assignment

--- client.js
+++ client.js
@@ -1,20 +1,23 @@
 const net = require("net");
+
 const connect = function() {
   const conn = net.createConnection({
     host:"localhost",
     port:50541
   });
+
   conn.setEncoding("utf8");
 
   conn.on("connect",()=>{
     console.log(`Successfully connected to game server`);
     conn.write(`Name: SKS`);
+    conn.write(`Move: up`);
   });
   
-
   conn.on("data",(data)=>{
     console.log(`Data for player:`, data);
   });
+
   return conn;
 };
 module.exports = connect;
