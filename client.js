0ff9b72f71145d5a9f03f90dc1ab27081a192b01
SimarpreetSidhu <simarpreetsidhu18@gmail.com>
2025-05-14 23:45:54 -0400

    complete snake initials assignment

--- client.js
+++ client.js
@@ -6,6 +6,12 @@ const connect = function() {
   });
   conn.setEncoding("utf8");
 
+  conn.on("connect",()=>{
+    console.log(`Successfully connected to game server`);
+    conn.write(`Name: SKS`);
+  });
+  
+
   conn.on("data",(data)=>{
     console.log(`Data for player:`, data);
   });
